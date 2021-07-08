4e1b8c11d879e0fcd21b3c93618c60d73ceb5738
Behzad <behzad@azarsa.com>
2021-07-08 11:57:04 +0200

    some changes

--- README.md
+++ README.md
@@ -1,15 +1,27 @@
+### Game Description
+
+The player will see a map of Europe without Streets + Cities (Only Country-borders). Its mission is to find the right location to the city name on this map. After placing the needle pin, the game will show you the right location of the city and the difference of your needle pin and the city in kilometers. If it is in around 50km of the city, the selection will be defined as "correct".
+
+### Game Logic
+
+In the beginning, the player has a predetermined score of 1500 that symbolizes kilometers. At each round the difference between position of the city and your needle pin are reducing your score.
+
+The game ends when no kilometers are left.
+The high score is the amount of cities you have found.
+
+![alt text](https://github.com/ibexad/game-map/blob/master/image.jpg?raw=true)
+
 ### Features
 
 - Using *React* 16.13.1
 - Responsive implementation on desktop and mobile browsers
 - Using *Hooks* for state management
-- Using *Local Storage* to store the highscore
-
+- Using *Local Storage* to store the high score
 
 ### Tested on
 
-- Chrome 86.0.4240.75 (Official Build) (32-bit)
-- FireFox 81.0.1 (64-bit)
-- Galaxy S5 360x640 (Vertical & Horizental)
-- iPhone 6/7/8 375x667 (Vertical & Horizental)
-- iPad
+- Chrome Version 91.0.4472.124 (Official Build) (64-bit)
+- Microsoft Edge Version 91.0.864.64 (Official build) (64-bit)
+- Galaxy S5 360x640 (Vertical & Horizontal)
+- iPhone 6/7/8 375x667 (Vertical & Horizontal)
+- iPad 2
--- src/pages/game.js
+++ src/pages/game.js
@@ -2,109 +2,135 @@ import React, { useState, useEffect } from 'react'
 import { Map, Marker, TileLayer, Tooltip } from 'react-leaflet'
 import data from "../data/capitalCities.json"
 import { useLocalStorage, distance } from "../utils"
+import { Button, Container, Row, Col, Alert, Badge, Navbar } from 'react-bootstrap'
 //import { icon } from "../components/icon";
- 
+
 const Game = () => {
 
-  const zoom = 5;  
+  const zoom = 5;
   const diffKilometer = 50;
-  const maxKilometer = 1500;  
+  const maxKilometer = 1500;
 
   const [position, setPosition] = useState([52.374647, 9.738464]); //kröpcke hannover as start location of map :)
   const [placed, setPlaced] = useState(0);
-  const [kilometers, setKilometers] = useState(maxKilometer);  
+  const [kilometers, setKilometers] = useState(maxKilometer);
   const [index, setIndex] = useState(0);
   const [markers, setMarkers] = useState([]);
   const [finished, setFinished] = useState(false);
   const [city, setCity] = useState(null);
+  const [showCorrectAlert, setShowCorrectAlert] = useState(false);
 
-  const [highscore,setHighscore] = useLocalStorage('highscore' , 0);
+  const [highScore,setHighScore] = useLocalStorage('highscore' , 0);
 
   useEffect(() => {
       if(index < data.capitalCities.length){
-          setCity(data.capitalCities[index].capitalCity);          
-      }               
-  },[index, city]);   
+          setCity(data.capitalCities[index].capitalCity);
+      }
+  },[index, city]);
 
-  const OnMapClick = (e) => {    
+  const OnMapClick = (e) => {
       const position = [e.latlng.lat, e.latlng.lng];
-      setPosition(position);  
+      setPosition(position);
       setMarkers([position]);
-  }  
+      setShowCorrectAlert(false);
+  }
 
   const OnPlaceButtonClick = () =>{
       const lat = parseFloat(data.capitalCities[index].lat);
       const long = parseFloat(data.capitalCities[index].long);
-      const currentPosition = [lat, long];  
-    
-      const kilometer = distance(lat, long, position[0], position[1]);  
+      const currentPosition = [lat, long];
+
+      const kilometer = distance(lat, long, position[0], position[1]);
+      setShowCorrectAlert(false);
 
       if(kilometer <= diffKilometer){
-        alert('correct!')
+        setShowCorrectAlert(true);
         if(!finished){
             setPlaced(placed + 1);
-        }        
-      }  
+        }
+      }
       else{
           setKilometers(kilometers - kilometer);
           setMarkers([position, currentPosition]);
-          
+
           if((kilometers - kilometer) <= 0){
               setKilometers(0);
               setFinished(true);
-              if(placed > highscore){
-                  setHighscore(placed);
+              if(placed > highScore){
+                  setHighScore(placed);
               }
           }
-      }      
+      }
+
+      setTimeout(() => {
+          OnNext();
+      }, 3000);
   }
 
-  const OnNextButtonClick = () =>{
+  const OnNext = () =>{
+      setShowCorrectAlert(false);
       setMarkers([]);
       setIndex(index + 1);
 
       if(index >= data.capitalCities.length){
           setFinished(true);
-          if(placed > highscore){
-              setHighscore(placed);
-          }        
+          if(placed > highScore){
+              setHighScore(placed);
+          }
       }
   }
 
   return(
-    <>
-        <div className='text-center'>
-            {placed} cities placed / High Score: {highscore}
+    <Container fluid className="vh-100 d-flex flex-column">
+        <Row>
+            <Col md={{ span: 4, offset: 4 }}>
+                <div className='p-1 mt-4 border border-secondary bg-light text-center'>
+                    {placed} cities placed / High Score: {highScore}
+                </div>
+                <div className='p-1 border border-secondary bg-light text-center'>
+                    {kilometers} kilometers left
+                </div>
+                <div className='text-center m-2'>
+                    {!finished ? <span>Select the location of <br/> "{city}"</span> : "Game Over!"}
+                </div>
+                <Alert show={showCorrectAlert} variant="success">Correct!</Alert>
+            </Col>
+        </Row>
+    <Row className="h-100">
+        <Col>
+        <div className="d-flex flex-column h-100">
+            <Map center={position} zoom={zoom} onclick={OnMapClick}>    {/* zoomControl={false} scrollWheelZoom={false} doubleClickZoom={false} */}
+                <TileLayer
+                url='https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}.png'
+                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                />
+                {markers.map((element, index) => (
+                <Marker
+                    key={index}
+                    marker_index={index}
+                    position={element}
+                    //icon={icon}
+                >
+                    {index !== 0 ? <Tooltip permanent={true}>{city} is here!</Tooltip> : null}
+                </Marker>
+                ))}
+            </Map>
         </div>
-        <div className='text-center'>
-            {kilometers} kilometers left
-        </div>
-        <div className='text-center'>Select the location of {<span className='badge badge-danger'>{city}</span>}</div>
-            
-        <Map center={position} zoom={zoom} onclick={OnMapClick}>    {/* zoomControl={false} scrollWheelZoom={false} doubleClickZoom={false} */}         
-            <TileLayer
-              url='https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}.png'
-              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-            />    
-              {markers.map((element, index) => (
-              <Marker
-                  key={index}
-                  marker_index={index}
-                  position={element}                
-                  //icon={icon}               
-              >             
-                {index !== 0 ? <Tooltip permanent={true}>{city} is here!</Tooltip> : null}
-              </Marker>
-              ))}     
-        </Map>
-        <div className='fixed-bottom'>        
-            <button onClick={OnPlaceButtonClick} className='btn btn-lg btn-primary float-right mr-2 mb-2'>Place</button>
-            <button disabled={finished} onClick={OnNextButtonClick} className='btn btn-lg btn-primary float-right mr-2 mb-2'>Next</button>
-        </div>      
-      </>
-  )  
+        </Col>
+    </Row>
+    <Row>
+        <Col className="d-flex align-items-end flex-column">
+            <div className="ml-auto py-2">
+                <Button variant="primary" disabled={finished} onClick={OnPlaceButtonClick}>Place</Button>
+                {/* <Button variant="secondary" onClick={OnNext}>Next</Button> */}
+            </div>
+        </Col>
+    </Row>
+  </Container>
+
+  )
 };
- 
+
 export default Game;
 
 
